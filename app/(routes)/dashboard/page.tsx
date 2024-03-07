e1ba6e0c314e02170206fc8cd790c523e9900bb6
priyanshukr449 <priyanshukr449@gmail.com>
2024-03-07 10:12:38 +0530

    protected routes added

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,8 +1,13 @@
+"use client"
+
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs'
 import React from 'react'
 
 const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
+    <div>DashboardPage
+      <LogoutLink>logout</LogoutLink>
+    </div>
   )
 }
 
---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+	const { isAuthenticated } = getKindeServerSession();
+	if (!(await isAuthenticated())) {
+		return NextResponse.redirect(
+			new URL(
+				"/api/auth/login?post_login_redirect_url=/dashboard",
+				request.url
+			)
+		);
+	}
+}
+
+// dashboard is a protected route now
+export const config = {
+	matcher: "/dashboard",
+};
